b2e7ac2ac23db76eef1b3458323e1991498a640e
신은찬 <dmscks3126@gmail.com>
2025-02-10 21:52:51 +0900

    25-02-10 TIL

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -1,30 +1,30 @@
 import { useState } from 'react'
 import './App.css'
 
-function App() {
-
-  let post = '강남 우동 맛집'
-  let [글제목, 글제목수정] = useState(['남자코트 추천', '강남 우동맛집', '파이썬독학'])
-  let [글내용, 글내용수정] = useState('2월 17일 발행')
+function App(){
+ 
+  let [글제목, 글제목변경] = useState( ['남자코트 추천', '강남 우동맛집', '파이썬 독학'] );
+  let [따봉, 따봉변경] = useState(0)
 
   return (
-      <div className="App">
-        <div className='black-nav'>
-          <h4>ReactBlog</h4>
-        </div>
-        <div className="list">
-        <h4>{ 글제목[0] }</h4>
-        <p>{ 글내용 }</p>
+    <div className="App">
+      <div className="black-nav">
+        <div>React blog</div>
       </div>
-        <div className="list">
+      <div className="list">
+        <h4>{ 글제목[0] } <span onClick={()=>{ 따봉변경(따봉+1) }}>👍</span> { 따봉 } </h4>
+        <p>2월 17일 발행</p>
+        <button onClick={()=>{ 글제목변경(글제목[0]='여자코트 추천') }}>글제목 변경</button>
+      </div>
+      <div className="list">
         <h4>{ 글제목[1] }</h4>
-        <p>{ 글내용 }</p>
+        <p>2월 17일 발행</p>
       </div>
-        <div className="list">
+      <div className="list">
         <h4>{ 글제목[2] }</h4>
-        <p>{ 글내용 }</p>
+        <p>2월 17일 발행</p>
       </div>
-      </div>
+    </div>
   )
 }
 
