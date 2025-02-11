42955fd24b6e89c059a793283ebcd46c879d2a9d
신은찬 <dmscks3126@gmail.com>
2025-02-11 22:16:35 +0900

    20250211 TIL

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -11,15 +11,31 @@ function App(){
       <div className="black-nav">
         <div>React blog</div>
       </div>
+
+        <button onClick={()=>{
+          
+          let copy = [...글제목]
+          copy[0] = '여자코트 추천'
+          글제목변경(copy) 
+          }}>글제목 수정</button>
+
+        <button onClick={()=> {
+          
+          let copy = [...글제목]
+          copy.sort()
+          글제목변경(copy)
+        }}>가나다 정렬</button>
+
       <div className="list">
         <h4>{ 글제목[0] } <span onClick={()=>{ 따봉변경(따봉+1) }}>👍</span> { 따봉 } </h4>
         <p>2월 17일 발행</p>
-        <button onClick={()=>{ 글제목변경(글제목[0]='여자코트 추천') }}>글제목 변경</button>
       </div>
+
       <div className="list">
         <h4>{ 글제목[1] }</h4>
         <p>2월 17일 발행</p>
       </div>
+
       <div className="list">
         <h4>{ 글제목[2] }</h4>
         <p>2월 17일 발행</p>
