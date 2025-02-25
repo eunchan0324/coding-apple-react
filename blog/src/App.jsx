ac6f8324268c09eb0aeee45a1560e6bd9ef28ba4
신은찬 <dmscks3126@gmail.com>
2025-02-25 17:50:47 +0900

    25-02-25 TIL

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -7,6 +7,7 @@ function App(){
   let [modal, setModal] = useState(false);
   let [따봉, 따봉변경] = useState([0,0,0])
   let [title, setTitle] = useState(0)
+  let [글발행, 글발행변경] = useState('')
   
   return (
     <div className="App">
@@ -14,53 +15,32 @@ function App(){
         <div>React blog</div>
       </div>
 
-        {/* <button onClick={()=>{
-          
-          let copy = [...글제목]
-          copy[0] = '여자코트 추천'
-          글제목변경(copy) 
-          }}>글제목 수정</button>
-
-        <button onClick={()=> {
-          
-          let copy = [...글제목]
-          copy.sort()
-          글제목변경(copy)
-        }}>가나다 정렬</button> */}
-
-      {/* <div className="list">
-        <h4>{ 글제목[0] } <span onClick={()=>{ 따봉변경(따봉+1) }}>👍</span> { 따봉 } </h4>
-        <p>2월 17일 발행</p>
-      </div>
-
-      <div className="list">
-        <h4>{ 글제목[1] }</h4>
-        <p>2월 17일 발행</p>
-      </div>
-
-      <div className="list">
-        <h4 onClick={()=> { setModal(!modal) }}>{ 글제목[2] }</h4>
-        <p>2월 17일 발행</p>
-      </div> */}
-
-
       {
         글제목.map(function(a, i){
           return(
             <div className="list" key={i}>
             <h4 onClick={()=> { setModal(true); setTitle(i) }}> 
               { 글제목[i] } 
-              <span onClick={()=>{ 
+              <span onClick={(e)=>{
+                  e.stopPropagation();
                   let copy = [...따봉];
                   copy[i] = copy[i] + 1;
                   따봉변경(copy)  
               }}>👍</span> {따봉[i]} 
             </h4> 
             <p>2월 17일 발행</p>
+            <button onClick={()=> {
+               글제목변경(글제목.filter((_, idx) => idx !== i))
+               }}>삭제</button>
           </div>
           )
         })
       }
+
+      <div>
+        <input onChange={(e)=> { 글발행변경(e.target.value); console.log(글발행) }}></input>
+        <button onClick={()=> { 글제목변경(글제목.concat(글발행))}}>글발행</button>
+      </div>
 
       {
         modal == true ? <Modal title={title} 글제목={글제목} 글제목변경={글제목변경}></Modal> : null
