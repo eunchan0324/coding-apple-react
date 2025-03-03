1e0fa74e584ed531a396089aa3538f6db6ca2cff
신은찬 <dmscks3126@gmail.com>
2025-03-03 14:51:43 +0900

    250303 TIL

--- shop/src/App.jsx
+++ shop/src/App.jsx
@@ -6,8 +6,11 @@ import Navbar from 'react-bootstrap/Navbar';
 import bg from './bg.png'
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import data from './data'
 
 function App() {
+
+  let [shoes, setShoes] = useState(data);
 
   return (
     <>
@@ -25,24 +28,20 @@ function App() {
       <div className="main-bg" style={{ backgroundImage : 'url('+ bg + ')' }}></div>
 
       <Container>
-      <Row>
-        <Col>
-          <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="80%"/>
-          <h4>상품명</h4>
-          <p>상품정보</p>
-        </Col>
-        <Col>
-          <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="80%"/>
-          <h4>상품명</h4>
-          <p>상품정보</p>
-        </Col>
-        <Col>
-          <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="80%"/>
-          <h4>상품명</h4>
-          <p>상품정보</p>
-        </Col>
-      </Row>
-    </Container>
+        <Row>
+            {
+              shoes.map(function(a, i){
+                return(
+                  <Col md={4} key={i}>
+                  <img src={`https://codingapple1.github.io/shop/shoes${i + 1}.jpg`} width="80%"/>
+                  <h4>{shoes[i].title}</h4>
+                  <p>{shoes[i].price}</p>
+                  </Col>
+                )
+              })
+            }
+        </Row>
+      </Container>
 
     </>
   )
