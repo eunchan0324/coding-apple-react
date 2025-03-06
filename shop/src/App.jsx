ee276660bd2e0496fbf10a8c3ff8eef083d00ecb
신은찬 <dmscks3126@gmail.com>
2025-03-06 16:30:43 +0900

    25-03-06 TIL

--- shop/src/App.jsx
+++ shop/src/App.jsx
@@ -48,11 +48,13 @@ function App() {
 
           </>
         } />
-        <Route path='/detail' element={ <Detail /> } />
+        <Route path='/detail/:id' element={ <Detail shoes={shoes} /> } />
+
         <Route path='/about' element={ <About /> }>
           <Route path='member' element={ <div>맴버임</div> } />
           <Route path='location' element={ <div>지역임</div> } />
         </Route>
+
         <Route path='/event' element={ <Event /> }>
           <Route path='one' element={ <div>첫 주문시 양배추즙 서비스</div> } />
           <Route path='two' element={ <div>생일기념 쿠폰받기</div> } />
--- shop/src/pages/detail.jsx
+++ shop/src/pages/detail.jsx
@@ -1,20 +1,27 @@
-function Detail() {
+import { useParams } from "react-router-dom";
+
+function Detail(props){
+
+  let { id } = useParams();
+  let 찾은상품 = props.shoes.find(function(x){
+    return x.id == id
+  });
+
   return (
     <div className="container">
       <div className="row">
         <div className="col-md-6">
           <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
         </div>
-        <div className="col-md-6">
-          <h4 className="pt-5">상품명</h4>
-          <p>상품설명</p>
-          <p>120000원</p>
-          <button className="btn btn-danger">주문하기</button>
+        <div className="col-md-6 mt-4">
+          <h4 className="pt-5">{찾은상품.title}</h4>
+          <p>{찾은상품.content}</p>
+          <p>{찾은상품.price}원</p>
+          <button className="btn btn-danger">주문하기</button> 
         </div>
       </div>
-    </div>
-
+  </div>  
   )
-}
+};
 
-export default Detail;
+export default Detail 
